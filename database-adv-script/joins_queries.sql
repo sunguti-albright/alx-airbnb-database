f0e9f9f239009e4ad450f3dbc14c51258a41fc43
Albright Sunguti <sungutialbright@gmail.com>
2025-07-10 10:40:28 +0300

    add order_by as per specs

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -9,7 +9,8 @@ SELECT
     u.last_name,
     u.email
 FROM booking b
-INNER JOIN "User" u ON b.user_id = u.user_id;
+INNER JOIN "User" u ON b.user_id = u.user_id
+ORDER BY u.first_name, u.last_name;
 
 -- 2. LEFT JOIN: Properties with their reviews (including properties without reviews)
 SELECT 
@@ -19,7 +20,8 @@ SELECT
     r.rating,
     r.comment
 FROM property p
-LEFT JOIN review r ON p.property_id = r.property_id;
+LEFT JOIN review r ON p.property_id = r.property_id
+ORDER BY p.name;
 
 -- 3. FULL OUTER JOIN: Users and Bookings (all, even if unmatched)
 SELECT 
@@ -31,5 +33,5 @@ SELECT
     b.end_date,
     b.status
 FROM "User" u
-FULL OUTER JOIN booking b ON u.user_id = b.user_id;
-
+FULL OUTER JOIN booking b ON u.user_id = b.user_id
+ORDER BY u.first_name, u.last_name;
